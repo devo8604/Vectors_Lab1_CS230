73f1bd4e9e61708d3d23b20767b9d0bf92e13dd9
devo8604 <dsmith@devo-macbook.devo.lan>
2014-07-07 20:31:04 -0600

    Initial Commit

---
+++ Vectors_Lab1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Vectors_Lab1
+//
+//  Created by Devon Smith on 7/7/14.
+//  Copyright (c) 2014 Devon Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
